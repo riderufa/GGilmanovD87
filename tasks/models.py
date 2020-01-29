2a8494823139bb496ca05eebe53d16905f80e6ee
Gilmanov Garri <3434455@mail.ru>
2020-01-29 23:00:36 +0500

    Add_model_Priority

--- tasks/models.py
+++ tasks/models.py
@@ -15,18 +15,19 @@ class Category(models.Model):
     def __str__(self):
         return f'{self.name} ({self.slug})'
 
+class Priority(models.Model):
+    name = models.CharField(max_length=50)
+    todos_count = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Приоритет'
+        verbose_name_plural = 'Приоритеты'
+
+    def __str__(self):
+        return self.name
 
 class TodoItem(models.Model):
-    PRIORITY_HIGH = 1
-    PRIORITY_MEDIUM = 2
-    PRIORITY_LOW = 3
-
-    PRIORITY_CHOICES = [
-        (PRIORITY_HIGH, "Высокий приоритет"),
-        (PRIORITY_MEDIUM, "Средний приоритет"),
-        (PRIORITY_LOW, "Низкий приоритет"),
-    ]
-
+    
     description = models.TextField("описание")
     is_completed = models.BooleanField("выполнено", default=False)
     created = models.DateTimeField(auto_now_add=True)
@@ -34,10 +35,15 @@ class TodoItem(models.Model):
     owner = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="tasks"
     )
-    priority = models.IntegerField(
-        "Приоритет", choices=PRIORITY_CHOICES, default=PRIORITY_MEDIUM
+    priority = models.ForeignKey(
+        Priority, on_delete=models.CASCADE, related_name="todo"
     )
     category = models.ManyToManyField(Category, blank=True)
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
 
     def __str__(self):
         return self.description.lower()
